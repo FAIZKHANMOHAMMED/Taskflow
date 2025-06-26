ea472326ae298e2fa3c5114e98d7885de9b961b2
FAIZ KHAN <nexusblade5195@gmail.com>
2025-06-26 23:01:56 +0530

    back url gave

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import { createRoot } from 'react-dom/client';
-import React from 'react';
 import App from './App';
 import './index.css';
 
@@ -7,9 +6,4 @@ const container = document.getElementById('root');
 if (!container) throw new Error('Failed to find the root element');
 
 const root = createRoot(container);
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
